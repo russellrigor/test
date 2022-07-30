b2a0a1522946ce10c4bc5672af1b4c3eb628766e
russellrigor <russellrigor4@gmail.com>
2022-07-29 19:55:37 -0700

    aaa

--- bot.js
+++ bot.js
@@ -4,15 +4,37 @@ const { clientId, guildId, token } = require('./config.json');
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const commands = [
+	new SlashCommandBuilder().setName('monday').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+const rest = new REST({ version: '9' }).setToken(token);
 
-client.on("message", msg => {
-    if (msg.content == "monday") {
-      msg.react("https://cdn.discordapp.com/attachments/837931604717469727/1002420914526367814/trim.DFA79055-B586-4752-A281-BD34328AFF4E.mov")
-    })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+        .then(() => console.log('Successfully registered application commands.'))
+        .catch(console.error);
 
 client.once('ready', () => {
     console.log('ready 2 go');
 });    
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'monday') {
+		await interaction.reply('https://cdn.discordapp.com/attachments/837931604717469727/1002420914526367814/trim.DFA79055-B586-4752-A281-BD34328AFF4E.mov');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
+
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
   
