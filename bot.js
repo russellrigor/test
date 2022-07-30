8db18bed364219ad987dd1ef682e278115d7ae11
russellrigor <russellrigor4@gmail.com>
2022-07-29 19:46:14 -0700

    aaa

--- bot.js
+++ bot.js
@@ -8,10 +8,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.on("message", msg => {
     if (msg.content == "monday") {
       msg.react("https://cdn.discordapp.com/attachments/837931604717469727/1002420914526367814/trim.DFA79055-B586-4752-A281-BD34328AFF4E.mov")
-    }
+    })
 
 client.once('ready', () => {
     console.log('ready 2 go');
 });    
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
 // Login to Discord with your client's token
 client.login(token);
+client.login(process.env.BOT_TOKEN)
